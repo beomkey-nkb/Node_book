28636662db92dc388534bc49ec2f38146fcf42c6
NamKiBeom <nkb5813@naver.com>
2019-01-23 02:31:42 +0900

    네이버 API 호출
    네이버 API를 사용하여 책 정보 JSON으로 받아오기
    -파싱은 아직 하지 않음
    -ISBN 추출 예정

--- test.js
+++ test.js
@@ -1,1 +1,21 @@
-console.log('test');
+var request = require('request');
+
+var options = {
+	headers : {
+		"X-Naver-Client-Id" : "S6Q53PE53pQn44KDWD1J",
+		"X-Naver-Client-Secret" : "br1oxDVW6X"
+	},
+	method : 'get',
+	encoding: "utf-8",
+	url : 'https://openapi.naver.com/v1/search/book.json',
+	qs : {
+    query : "Ionic 프로그래밍 입문",
+	  display : 1,
+	  start : 1,
+    sort : "sim",
+	}
+}
+
+request(options, function(err, res, html) {
+	console.log(html);
+});
