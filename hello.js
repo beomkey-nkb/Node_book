670d4c635c67110e82ff51fbac3cdba801408967
kingshinwook <46843095+kingshinwook@users.noreply.github.com>
2019-01-24 15:30:54 +0900

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,12 +1,1 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 1337;
-
-http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-console.log('hi my name is shinwook');
-});
+console.log('asdasdasd');
