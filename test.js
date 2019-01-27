d65923a3cc25d7d8cf6deadd369136f3fedbb175
NamKiBeom <nkb5813@naver.com>
2019-01-28 02:03:17 +0900

    API 활용 완성
    알라딘 API 활용 불가로 인하여 yes24 웹페이지를 파싱하는 방식으로 대처
    
    순서
    네이버API->isbn추출->yes24검색페이지 파싱->url추출->최종html파싱->중고가격 추출
    
    -주석을 적지  않아서 주석을 추가가 필요함

--- test.js
+++ test.js
@@ -1,21 +1,72 @@
-var request = require('request');
+var request = require('request-promise');
+var await = require('await');
+var https = require('https');
+var fs = require('fs'),
+express = require('express'),
+http = require('http');
+var iconv = require('iconv-lite');
+var charset = require('charset');
+var cheerio = require('cheerio');
+var async = require('async');
+var buffer = require('buffer/').Buffer
 
-var options = {
-	headers : {
-		"X-Naver-Client-Id" : "S6Q53PE53pQn44KDWD1J",
-		"X-Naver-Client-Secret" : "br1oxDVW6X"
-	},
-	method : 'get',
-	encoding: "utf-8",
-	url : 'https://openapi.naver.com/v1/search/book.json',
-	qs : {
-    query : "Ionic 프로그래밍 입문",
-	  display : 1,
-	  start : 1,
-    sort : "sim",
-	}
-}
+var app = express();
+app.set('port', process.env.PORT || 3000);
 
-request(options, function(err, res, html) {
-	console.log(html);
+app.use(function(req, res, next){
+	console.log('첫번째 미들웨어 요청 처리');
+	var options = {
+		headers : {
+			"X-Naver-Client-Id" : "S6Q53PE53pQn44KDWD1J",
+			"X-Naver-Client-Secret" : "br1oxDVW6X"
+		},
+		method : 'get',
+		encoding: "utf-8",
+		url : 'https://openapi.naver.com/v1/search/book.json',
+		qs : {
+		query : "Ionic프로그래밍",
+		  display : 1,
+		  start : 1,
+		sort : "sim",
+		}
+	};
+	
+	var result = request.get(options,function(req,rek,body){
+		var parser = JSON.parse(body);
+		var isbnString = parser.items[0].isbn.slice(11,24);
+		console.log(isbnString);
+		var options1 = {
+			url: 'http://www.yes24.com/searchcorner/Search?keywordAd=&keyword=&qdomain=UsedGoods&query='+isbnString+'&domain=USED_GOODS&scode=006_015'
+			,encoding : null
+			
+		}
+		async function crawler(){
+			var doc = iconv.decode(new buffer(await request(options1)),'EUC-KR').toString();
+			
+			var $1 = cheerio.load(doc);
+			
+			var finalurl = ''+$1('.btn_view_allused').attr('href').toString();
+			console.log(finalurl);
+			
+			var finalOption = {
+				url: finalurl,
+				encoding : null
+			}
+
+			var finaldoc = iconv.decode(new buffer(await request(finalurl)),'EUC-KR').toString();
+		
+			var $ = cheerio.load(finaldoc);
+
+			var finaldata = $('.yes_b').text().slice(0,6);
+
+			res.send(finaldata);
+		};
+
+		crawler();
+	
+	});
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('express server start : '+app.get('port'));
 });
