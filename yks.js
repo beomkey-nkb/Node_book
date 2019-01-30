032a345c8a8998be8bd1a53eba2b68b612210a86
yoo-kyoung-su <46843064+yoo-kyoung-su@users.noreply.github.com>
2019-01-30 18:17:33 +0900

    서버 개설 및 json데이터값 출력
    앱에서 출력가능한 타입 선언

--- yks.js
+++ yks.js
@@ -1,1 +1,26 @@
-console.log('1,2,3');
+﻿var express = require('express');
+var http = require('http');
+
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.use(function(req,res,next) {
+   console.log('첫번째 미들웨어 호출');
+    
+    //json데이터 통신을 위한 예시
+    var book = {ISBN:'132312',name:'무선통신'};
+    var bookstr = JSON.stringify(book);
+    res.writeHead('200', {"Content-Type":"application/json;charset=utf8"})
+    res.write(bookstr);
+    res.end();
+});
+
+  
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log('웹 서버 실행:' + app.get('port'));
+});
+
