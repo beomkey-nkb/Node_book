25af6c6562635c135f7c75483dc7c347f21d5f50
kingshinwook <46843095+kingshinwook@users.noreply.github.com>
2019-02-01 09:34:51 +0900

    노드-sql연동

--- hello.js
+++ hello.js
@@ -1,6 +1,16 @@
-function hello(name)
-{
-console.log('hi,'+name);
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',//이거 광역서버로 할거면 openhost
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
 
-}
-hello('engoing');
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
