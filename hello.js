a18d38147526b33b88f0ce875df007c6509d23b5
kingshinwook <46843095+kingshinwook@users.noreply.github.com>
2019-02-01 09:41:09 +0900

    node-sql ....3

--- hello.js
+++ hello.js
@@ -1,10 +1,10 @@
 var mysql      = require('mysql');
 var connection = mysql.createConnection({
-  host     : 'localhost',//
+  host     : 'localhost',//접속할 데이터베이스 주소
   user     : 'root',//사용자 이름
   password : '111111',//호스트 비밀번호
   database : 'o2'// 내가 설정한 데이터베이스 이름
-});
+});//이거 원래는 보안상으로 이렇게하면안댐 현재는 내 컴퓨터랑 맞추는 중
 
 connection.connect();
 
