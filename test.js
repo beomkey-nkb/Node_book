62be6c1d676650a9986b24c8e40c8788b7c54d6c
kingshinwook <46843095+kingshinwook@users.noreply.github.com>
2019-02-01 09:35:51 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-console.log('test');
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',//이거 광역서버로 할거면 openhost
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
