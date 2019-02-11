64a847e2ae4182e586435c0027406bef02d2b78b
yoo-kyoung-su <46843064+yoo-kyoung-su@users.noreply.github.com>
2019-02-11 20:36:15 +0900

    client<->server 통신코드
    기범이 코드와 연계하여 get형식의 url생성

--- yks.js
+++ yks.js
@@ -1,26 +1,85 @@
-﻿var express = require('express');
-var http = require('http');
+﻿var request = require('request-promise');
+var await = require('await');
+var https = require('https');
+var fs = require('fs'),
+express = require('express'),
+http = require('http');
+var iconv = require('iconv-lite');
+var charset = require('charset');
+var cheerio = require('cheerio');
+var async = require('async');
+var buffer = require('buffer/').Buffer
+var url = require('url');
 
 
 var app = express();
-
 app.set('port', process.env.PORT || 3000);
 
+app.use(function(req, res, next){
+     if(req.url == '/'){
+      url = '/index.html';
+    }
+    if(req.url == '/favicon.ico'){
+      return res.writeHead(404);
+    }
+	console.log('첫번째 미들웨어 요청 처리');
+    var _url = req.url;
+   var querydata = url.parse(_url,true).query;
+    console.log(querydata.name);
+	var options = {
+		headers : {
+			"X-Naver-Client-Id" : "S6Q53PE53pQn44KDWD1J",
+			"X-Naver-Client-Secret" : "br1oxDVW6X"
+		},
+		method : 'get',
+		encoding: "utf-8",
+		url : 'https://openapi.naver.com/v1/search/book.json',
+		qs : {
+		query : querydata.name,
+		  display : 1,
+		  start : 1,
+		sort : "sim",
+		}
+	};
+	
+	var result = request.get(options,function(req,rek,body){
+		var parser = JSON.parse(body);
+		var isbnString = parser.items[0].isbn.slice(11,24);
+		console.log(isbnString);
+		var options1 = {
+			url: 'http://www.yes24.com/searchcorner/Search?keywordAd=&keyword=&qdomain=UsedGoods&query='+isbnString+'&domain=USED_GOODS&scode=006_015'
+			,encoding : null
+			
+		}
+		async function crawler(){
+			var doc = iconv.decode(new buffer(await request(options1)),'EUC-KR').toString();
+			
+			var $1 = cheerio.load(doc);
+			
+			var finalurl = ''+$1('.btn_view_allused').attr('href').toString();
+			console.log(finalurl);
+			
+			var finalOption = {
+				url: finalurl,
+				encoding : null
+			}
 
-app.use(function(req,res,next) {
-   console.log('첫번째 미들웨어 호출');
-    
-    //json데이터 통신을 위한 예시
-    var book = {ISBN:'132312',name:'무선통신'};
-    var bookstr = JSON.stringify(book);
-    res.writeHead('200', {"Content-Type":"application/json;charset=utf8"})
-    res.write(bookstr);
-    res.end();
-});
+			var finaldoc = iconv.decode(new buffer(await request(finalurl)),'EUC-KR').toString();
+		
+			var $ = cheerio.load(finaldoc);
 
+			var finaldata = $('.yes_b').text().slice(0,6);
+
+			res.end(finaldata);
+		};
+
+		crawler();
+	
+	});
   
-
-var server = http.createServer(app).listen(app.get('port'), function(){
-    console.log('웹 서버 실행:' + app.get('port'));
 });
+    
 
+http.createServer(app).listen(app.get('port'), function(){
+	console.log('express server start : '+app.get('port'));
+});
