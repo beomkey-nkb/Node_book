14194b580bc3ba65eac7365a008f87f0f8612101
kingshinwook <46843095+kingshinwook@users.noreply.github.com>
2019-03-04 19:23:30 +0900

    Create 뻘짓.js

---
+++ 뻘짓.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var _storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+})
+var upload = multer({ storage: _storage })
+var fs = require('fs');
+var mysql = require('mysql');
+var conn = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '111111',
+  database : 'o2'
+});
+conn.connect();
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.locals.pretty = true;
+app.use('/user', express.static('uploads'));
+app.set('views', './views_mysql');
+app.set('view engine', 'jade');
+app.get('/upload', function(req, res){
+  res.render('upload');
+});
+app.post('/upload', upload.single('userfile'), function(req, res){
+  res.send('Uploaded : '+req.file.filename);
+});
+app.get('/topic/new', function(req, res){
+  fs.readdir('data', function(err, files){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.render('new', {topics:files});
+  });
+});
+app.get(['/topic', '/topic/:id'], function(req, res){
+  var sql = 'SELECT id,title FROM topic';
+  conn.query(sql, function(err, topics, fields){
+    var id = req.params.id;
+    if(id){
+      var sql = 'SELECT * FROM topic WHERE id=?';
+      conn.query(sql, [id], function(err, topic, fields){
+        if(err){
+          console.log(err);
+          res.status(500).send('Internal Server Error');
+        } else {
+          res.render('view', {topics:topics, topic:topic[0]});
+        }
+      });
+    } else {
+      res.render('view', {topics:topics});
+    }
+  });
+});
+app.post('/topic', function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  fs.writeFile('data/'+title, description, function(err){
+    if(err){
+      console.log(err);
+      res.status(500).send('Internal Server Error');
+    }
+    res.redirect('/topic/'+title);
+  });
+})
+app.listen(3000, function(){
+  console.log('Connected, 3000 port!');
+})
